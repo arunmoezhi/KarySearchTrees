c0194095e2238b3ff7ad8e8d5b48020b36c362e9
arunmoezhi <arunmoezhi@gmail.com>
2013-11-16 00:26:12 -0600

    updated to have <key,value> pair

--- src/Node.java
+++ src/Node.java
@@ -7,6 +7,7 @@ public class Node
 	public static final int NUM_OF_KEYS_IN_A_NODE= K-1;
 	public static final int NUM_OF_CHILDREN_FOR_A_NODE= K;
 	long keys[];
+	long values[];
 	Node[] childrenArray;
 	AtomicBoolean isLocked;
 	boolean isMarked;
@@ -23,12 +24,14 @@ public class Node
 	}
 	
 	
-	public Node(long[] keys, String nodeType)
+	public Node(long[] keys, long[] values,String nodeType)
 	{
 		this.keys = new long[NUM_OF_KEYS_IN_A_NODE];
+		this.values = new long[NUM_OF_KEYS_IN_A_NODE];
 		for(int i=0;i<NUM_OF_KEYS_IN_A_NODE;i++)
 		{
 			this.keys[i] = keys[i];
+			this.values[i] = values[i];
 		}
 		if(nodeType.equalsIgnoreCase("internalNode"))
 		{
