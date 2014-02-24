31e7f5924b1657661befe2cde7f5419869bb2e7b
Arunmoezhi Ramachandran <arunmoez@login3.stampede.tacc.utexas.edu>
2014-02-24 02:42:57 -0600

    initial commit

---
+++ src/header.h
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<limits.h>
+#include<math.h>
+#include<tbb/atomic.h>
+
+#define K 2
+#define NUM_OF_KEYS_IN_A_NODE K-1
+#define NUM_OF_CHILDREN_FOR_A_NODE K
+
+void createHeadNodes();
+unsigned long lookup(unsigned long);
+bool insert(unsigned long , unsigned long );
+bool remove(unsigned long);
+unsigned long size();
+void printKeys();
