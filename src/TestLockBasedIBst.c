152913d279ad298754fb481da7dc3f137d37bd66
arunmoezhi <arunmoezhi@gmail.com>
2014-03-12 01:43:32 -0500

    added a macro to XOR UINTPTR_MAX with 1

--- src/TestLockBasedIBst.c
+++ src/TestLockBasedIBst.c
@@ -1,17 +1,39 @@
 #include"header.h"
+#define ATOMIC_COUNTERS
 int NUM_OF_THREADS;
 int findPercent;
 int insertPercent;
 int deletePercent;
 unsigned long iterations;
 unsigned long keyRange;
+double* timeArray;
+double MOPS;
+#ifdef ATOMIC_COUNTERS
 tbb::atomic<int> totalInserted;
 tbb::atomic<int> totalDeleted;
+#endif
 
-void *operateOnTree(void *threadId)
+struct timespec diff(timespec start, timespec end)
 {
+	struct timespec temp;
+	if ((end.tv_nsec-start.tv_nsec)<0) 
+	{
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+	} else 
+	{
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
+}
+
+void *operateOnTree(void* threadId)
+{
+  struct timespec s,e;
   int chooseOperation;
   srand((long) threadId);
+  clock_gettime(CLOCK_REALTIME,&s);
   for(int i=0;i<iterations;i++)
   {
     chooseOperation = rand()%100;
@@ -21,25 +43,36 @@ void *operateOnTree(void *threadId)
     }
     else if (chooseOperation < insertPercent)
     {
+      #ifdef ATOMIC_COUNTERS
       totalInserted.fetch_and_add(insert(rand()%keyRange + 1,rand()%keyRange + 1));
+      #else
+      insert(rand()%keyRange + 1,rand()%keyRange + 1);
+      #endif
     }
     else
     {
+      #ifdef ATOMIC_COUNTERS
       totalDeleted.fetch_and_add(remove(rand()%keyRange + 1));
+      #else
+      remove(rand()%keyRange + 1);
+      #endif
     }
   }
+  clock_gettime(CLOCK_REALTIME,&e);
+  timeArray[(intptr_t) threadId] = (double) (diff(s,e).tv_sec * 1000000000 + diff(s,e).tv_nsec)/1000;
 }
 
 int main(int argc, char *argv[])
 {
+  double totalTime=0;
+  double avgTime=0;
   NUM_OF_THREADS = atoi(argv[1]);
   findPercent = atoi(argv[2]);
   insertPercent= findPercent + atoi(argv[3]);
   deletePercent = insertPercent + atoi(argv[4]);
   iterations = (unsigned long) pow(2,atoi(argv[5]));
   keyRange = (unsigned long) pow(2,atoi(argv[6]));
-  printf("%d\t%d\t%d\t%d\t%lu\t%lu\n",NUM_OF_THREADS,findPercent,insertPercent,deletePercent,iterations,keyRange);
-
+  timeArray = (double*)malloc(NUM_OF_THREADS * sizeof(double));
   createHeadNodes();
   for(int i=0;i<keyRange/2;i++)
   {
@@ -47,21 +80,28 @@ int main(int argc, char *argv[])
   }
   #ifdef DEBUG_ON
   printKeys();
-  #endif
   printf("Initial Size: %lu\n",size());
+  #endif
   pthread_t threadArray[NUM_OF_THREADS];
   for(int i=0;i<NUM_OF_THREADS;i++)
   {
-    pthread_create(&threadArray[i], NULL, operateOnTree, (void *)i);
+    pthread_create(&threadArray[i], NULL, operateOnTree, (void*) i);
   }
   for(int i=0;i<NUM_OF_THREADS;i++)
   {
     pthread_join(threadArray[i], NULL);
   }
+  #ifdef DEBUG_ON
   printf("Final Size: %lu\n",size());
   printf("Inserted %d\tDeleted %d\n",(int) totalInserted,(int) totalDeleted);
-  #ifdef DEBUG_ON
   printKeys();
   #endif
+  for(int i=0;i<NUM_OF_THREADS;i++)
+  {
+    totalTime += timeArray[i];
+  }
+  avgTime = totalTime/NUM_OF_THREADS;
+  MOPS = iterations*NUM_OF_THREADS/(avgTime);
+  printf("k%d;%d-%d-%d;%d;%ld;%.2f;%.2f\n",atoi(argv[6]),findPercent,(insertPercent-findPercent),(deletePercent-insertPercent),NUM_OF_THREADS,size(),avgTime/1000,MOPS);
   pthread_exit(NULL);
 }
--- src/header.h
+++ src/header.h
@@ -4,10 +4,11 @@
 #include<stdbool.h>
 #include<limits.h>
 #include<math.h>
+#include<time.h>
 #define __STDC_LIMIT_MACROS
 #include<stdint.h>
 #include<tbb/atomic.h>
-
+#define UINTPTR_MAX_XOR_WITH_1 (uintptr_t) (UINTPTR_MAX ^ 1)
 #define K 2
 #define NUM_OF_KEYS_IN_A_NODE K-1
 #define NUM_OF_CHILDREN_FOR_A_NODE K
